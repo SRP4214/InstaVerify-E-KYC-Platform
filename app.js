d9320e7c03adf377b08276ee76eb7c93275c80b6
rachitshah07 <rachitshah1525@gmail.com>
2024-08-24 19:41:44 +0530

    Constructed an ApiResponse constructor, added a password hashing util

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+dotenv.config({ path: "./process.env" });
+const port = process.env.PORT || 8204;
+try {
+  const db = require("./src/database/dbConfig");
+  db();
+} catch (error) {
+  console.log("Following error occured", error);
+}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: "httpp://localhost:3000" }));
+// Returns middleware that only parses json and only looks at requests where the
+// Content-Type header matches the type option.
+app.use(express.json());
+
+app.use("/api/user", require("./src/routes/userRoutes"));
+app.listen(port, async () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
