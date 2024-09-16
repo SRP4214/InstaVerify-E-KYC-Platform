b83ac4170a26c382f47bd977ee891e0a8704b454
rachitshah07 <rachitshah1525@gmail.com>
2024-09-16 21:53:33 +0530

    Removed transaction handling, solved circular structurewhile returning response

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -3,30 +3,26 @@ const UserService = require("../services/userService");
 const mongoose = require("mongoose");
 
 async function registerUser(req, res, next) {
-  const session = await mongoose.startSession();
-  session.startTransaction();
   console.log("registerUser Controller received Request", req.body);
-  try {
-    // Pass the session to the service function
-    await UserService.createUser(req, res, next, session).then((result) => {
-      console.log("registerUser controller results", result);
-      return res.status(201).json(new ApiResponse(201, "User created successfully", result));
-    });
-    await session.commitTransaction();
-  } catch (error) {
-    await session.abortTransaction();
-    console.error("Transaction aborted due to error:", error);
-    next(error); // Pass the error to error-handling middleware
-  } finally {
-    session.endSession();
-  }
+
+  // Pass the session to the service function
+  await UserService.createUser(req, res, next).then((result) => {
+    console.log("registerUser controller results", result);
+    return res.status(result.statusCode).send(result);
+  });
 }
 
 async function getUserById(req, res, next) {
-  console.log("getUserById Controller received Request", req.body, req.params);
-  UserService.getUserById(res, req.params.id).then((result) => {
-    console.log("getUserByID controller results", result);
-    res.status(result.statusCode).send(result);
-  });
+  console.log("getUserById Controller received Request", req.params);
+  try {
+    // const result = await UserService.getUserById(res, req.params.id);
+    UserService.getUserById(res, req.params.userID).then((result) => {
+      console.log("getUserByID controller results", result);
+      res.status(result.statusCode).send(result);
+    });
+  } catch (error) {
+    next(error);
+  }
 }
+
 module.exports = { registerUser, getUserById };
